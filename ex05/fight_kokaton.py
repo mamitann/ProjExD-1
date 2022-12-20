a718a658b0437d22857d0d3561a7f7de28cf42fc
moriya <c0a211271f@edu.teu.ac.jp>
2022-12-20 17:07:33 +0900

    戦うこうかとん

---
+++ ex05/fight_kokaton.py
@@ -0,0 +1,182 @@
+import pygame as pg
+import random
+import sys
+import os
+
+MAX_SHOTS = 2  # most player bullets onscreen
+ALIEN_ODDS = 22  # chances a new alien appears
+BOMB_ODDS = 60  # chances a new bomb will drop
+ALIEN_RELOAD = 12  # frames between new aliens
+SCREENRECT = pg.Rect(0, 0, 640, 480)
+SCORE = 0
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+class Screen:
+    def __init__(self, title, wh, img_path):
+        pg.display.set_caption(title) 
+        self.sfc = pg.display.set_mode(wh)
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(img_path)
+        self.bgi_rct = self.bgi_sfc.get_rect() 
+
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct) 
+
+
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img_path, ratio, xy):
+        self.sfc = pg.image.load(img_path)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        key_dct = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]  
+            if check_bound(self.rct, scr.rct) != (+1, +1):
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)                    
+
+
+
+def load_sound(file): 
+    """because pygame can be be compiled without mixer."""
+    if not pg.mixer:
+        return None
+    file = os.path.join(main_dir, "data", file)
+    try:
+        sound = pg.mixer.Sound(file)
+        return sound
+    except pg.error:
+        print("Warning, unable to load, %s" % file)
+    return None
+
+class Shot(pg.sprite.Sprite):
+    """a bullet the Player sprite fires."""
+
+    speed = -11
+    images = []
+
+    def __init__(self, pos):
+        pg.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(midbottom=pos)
+
+    def update(self):
+        """called every time around the game loop.
+
+        Every tick we move the shot upwards.
+        """
+        self.rect.move_ip(0, self.speed)
+        if self.rect.top <= 0:
+            self.kill()
+
+class Bomb:
+    def __init__(self, color, rad, vxy, scr:Screen):
+        self.sfc = pg.Surface((2*rad, 2*rad)) # 正方形の空のSurface
+        self.sfc.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.sfc, color, (rad, rad), rad)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+def check_bound(obj_rct, scr_rct):
+    """
+    第1引数：こうかとんrectまたは爆弾rect
+    第2引数：スクリーンrect
+    範囲内：+1／範囲外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
+
+
+def main():
+    clock =pg.time.Clock()
+
+    # 練習１
+    scr = Screen("逃げろ！こうかとん", (1600,900), "fig/pg_bg.jpg")
+
+    # 練習３
+    kkt = Bird("fig/6.png", 2.0, (900,400))
+    kkt.update(scr)
+
+    # 練習５
+    bkd = Bomb((255, 0, 0), 10, (+1, +1), scr)
+    bkd.update(scr)
+
+    boom_sound = load_sound("boom.wav")
+    shoot_sound = load_sound("car_door.wav")
+    if pg.mixer:
+        music = os.path.join(main_dir, "data", "house_lo.wav")
+        pg.mixer.music.load(music)
+        pg.mixer.music.play(-1)
+
+    """bombs = []
+    colors = ["red", "green", "blue", "yellow", "magenta"]
+
+    for i in range(5):
+        color = colors[i]
+        vx = random.choice([-1, +1])
+        vy = random.choice([-1, +1])
+        bombs.append(Bomb(color, 10, (vx, vy), Screen))
+    
+    Bird.update(Screen)
+    for bomb in bombs:
+        bomb.update(Screen)
+        if Bird.rct.colliderect(bomb.rct):
+            return"""
+
+
+    # 練習２
+    while True:        
+        scr.blit()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        kkt.update(scr)
+        bkd.update(scr)
+        if kkt.rct.colliderect(bkd.rct):
+            return
+
+        pg.display.update()
+        clock.tick(1000)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
